268774adb4180a3c25370afe6148fe214c21b01e
twisp007 <28303082+twisp007@users.noreply.github.com>
2021-05-24 00:27:11 +0530

    Create env.py

---
+++ env.py
@@ -0,0 +1,10 @@
+#these reddit credentials are used by the bot.
+#praw is used, see here for more info : https://praw.readthedocs.io/en/latest/getting_started/authentication.html
+
+client_id = ''
+client_secret = ''
+
+user_agent = ''
+
+username = ''
+password = ''
